c09de8a434db6582501151ff14552a90222ebaa0
Punkun_ <61596504+PunkunHm@users.noreply.github.com>
2024-07-14 00:52:52 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,40 @@
+function changeToName(event) {
+    event.preventDefault();
+    var input = document.getElementById("myInput");
+    var text = document.createElement("span");
+    text.textContent = input.value;
+    input.replaceWith(text);
+}
+
+// For the reveal effects
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
